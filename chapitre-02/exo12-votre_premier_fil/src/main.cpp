a56425427460d878cb9a9f4730fbc1d929476b4d
Ndzouli947 <abandawalter8@gmail.com>
2026-09-24 08:26:54 +0100

    Add files via upload

---
+++ chapitre-02/exo12-votre_premier_fil/src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>  
+#include<chrono>
+#include<cmath>
+
+
+int main ()
+{
+    const int N = 1000000;
+    double resultat = 0.0;
+    
+    // debut du chronomètrage
+    auto debut = std::chrono::high_resolution_clock::now();
+
+    // Calcul lourd 
+    for (int i = 1; i <= N; ++i) {
+        resultat += std::sqrt(static_cast <double> (i));
+    }
+
+    // Fin du chronomètrage 
+    auto fin = std::chrono::high_resolution_clock::now();
+
+    // calcul de la durée en millisecondes
+    auto duree = std::chrono::duration_cast < std::chrono::milliseconds>(
+        fin- debut
+    );
+    std::cout << "Resultat : " << resultat << std::endl;
+    std::cout << "Temps d'exécutoion : " << duree.count() << "ms" << std::endl;
+
+    return 0;
+}
